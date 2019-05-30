50a54796a2718c2874a285411e0209e3108797d8
Maia Teegarden <padmaia.thedev@gmail.com>
2019-05-29 17:51:41 -0700

    Rename Config to ParcelConfig

--- packages/core/core/src/PackagerRunner.js
+++ packages/core/core/src/PackagerRunner.js
@@ -3,7 +3,7 @@
 import {Readable} from 'stream';
 import type {ParcelOptions, Blob, FilePath} from '@parcel/types';
 import type {Bundle as InternalBundle} from './types';
-import type Config from './Config';
+import type Config from './ParcelConfig';
 import type InternalBundleGraph from './BundleGraph';
 
 import invariant from 'assert';
